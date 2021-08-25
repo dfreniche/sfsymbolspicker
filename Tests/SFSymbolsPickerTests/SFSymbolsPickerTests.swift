08017e9e9ab3312c902dba88e6cf74a657ca071d
Diego Freniche <diego@freniche.com>
2021-08-25 19:01:21 +0200

    Initial Commit

---
+++ Tests/SFSymbolsPickerTests/SFSymbolsPickerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SFSymbolsPicker
+
+final class SFSymbolsPickerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SFSymbolsPicker().text, "Hello, World!")
+    }
+}
